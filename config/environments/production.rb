e9ed11c61019af3cf315c1a1f8615a14be238015
yuji05225818@gmail.com <yuji05225818@gmail.com>
2017-01-27 20:51:27 +0900

    production.rbのメールドメイン設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
+  config.action_mailer.default_url_options = { host: 'radiant-inlet-42169.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
